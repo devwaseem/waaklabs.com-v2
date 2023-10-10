9bba8a93b43c04d1f681ca5247a85613e8dfab57
Waseem Akram <waseem07799@gmail.com>
2023-10-10 23:20:18 +0530

    Add chat interface

---
+++ src/components/chats/types.d.ts
@@ -0,0 +1,47 @@
+import type { ImageMetadata } from "astro";
+import type { HTMLAttributes } from "astro/types";
+
+export interface ChatSessionType {
+    timeAgo?: string;
+    sections: ChatSectionType[];
+}
+
+export interface ChatAuthorType {
+    image: ImageMetadata;
+    name: string;
+    side: "left" | "right";
+}
+
+export interface ChatSectionType {
+    author: ChatAuthorType;
+    messages: ChatMessageType[];
+}
+
+interface ChatBubbleProjectType {
+    name: string;
+    description: string;
+    link?: string;
+}
+interface ChatBubbleSocialType {
+    icon: ImageMetadata;
+    name: string;
+    link: string;
+}
+
+export interface ChatMessageType {
+    type:
+        | "text"
+        | "link"
+        | "image"
+        | "pricing"
+        | "project"
+        | "text-html"
+        | "real-chat"
+        | "social-link"
+        | "calendly";
+    html?: HTMLAttributes;
+    text?: string;
+    link?: ChatMessageLinkType;
+    project?: ChatBubbleProjectType;
+    social?: ChatBubbleSocialType;
+}
---
+++ src/data/projects.ts
@@ -0,0 +1,55 @@
+import type { ChatMessageType } from "../components/chats/types";
+export const chatProjects: ChatMessageType[] = [
+    {
+        type: "project",
+        project: {
+            name: "Contento AI",
+            description:
+                "We've made hiring a breeze with this Video Resume Platform. It lets job-seekers submit video resumes, which makes life easier for recruiters to spot the right fit quickly.",
+        },
+    },
+    {
+        type: "project",
+        project: {
+            name: "WeBridge",
+            link: "https://app.webridge.app",
+            description:
+                "We've made hiring a breeze with this Video Resume Platform. It lets job-seekers submit video resumes, which makes life easier for recruiters to spot the right fit quickly.",
+        },
+    },
+    {
+        type: "project",
+        project: {
+            name: "JobSkills",
+            description: "Coming Soon...",
+        },
+    },
+    {
+        type: "project",
+        project: {
+            name: "Adwiser",
+            description: "Coming Soon...",
+        },
+    },
+    {
+        type: "project",
+        project: {
+            name: "WAAK Labs v1",
+            description: "Coming Soon...",
+        },
+    },
+    {
+        type: "project",
+        project: {
+            name: "Laagom",
+            description: "Coming Soon...",
+        },
+    },
+    {
+        type: "project",
+        project: {
+            name: "Zeesh",
+            description: "Coming Soon...",
+        },
+    },
+];
