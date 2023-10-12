17fb26b7da9f6f03ad8b7608d91047270473ae7a
Waseem Akram <waseem07799@gmail.com>
2023-10-12 11:14:12 +0530

    feat: add seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
+  site: 'https://waaklabs.com',
+  integrations: [tailwind(), sitemap(), compressor()]
 });
